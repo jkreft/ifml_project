033d2319b66291f4a2337406d1998ea672096ba0
phaetjay <me@jkreft.de>
2019-03-19 20:24:18 +0100

    trying to get cuda running

--- agent_code/dqn_agent/callbacks.py
+++ agent_code/dqn_agent/callbacks.py
@@ -234,6 +234,7 @@ def setup(self):
         self.trainingstep = 1
         self.model.learningstep = 1
         self.analysis = []
+        self.steploss = 100
 
     self.model.explay = self.explay
     self.targetmodel.explay = self.explay
